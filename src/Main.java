8415398cc80f162bb158969c1fd1ffd8a1ed46e8
33 <1554470705@qq.com>
2024-06-21 16:13:40 +0800

    Day01

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: gs_wang
+ * @Date: 2024/06/21/下午4:12
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello JavaWeb!");
+    }
+}
