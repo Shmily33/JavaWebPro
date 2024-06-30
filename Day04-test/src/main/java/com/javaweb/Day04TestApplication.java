6e7307ff1bba42c2683eee111d02cd339c0bc169
33 <1554470705@qq.com>
2024-06-30 20:48:16 +0800

    Day05

--- Day04-test/src/main/java/com/javaweb/Day04TestApplication.java
+++ Day04-test/src/main/java/com/javaweb/Day04TestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day04TestApplication {
-
+    // 启动
     public static void main(String[] args) {
         SpringApplication.run(Day04TestApplication.class, args);
     }
